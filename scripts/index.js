c52681feac86a0bc5f62b31ad58f954c9aad418c
Alexey Kuznetsov <i@akzntsv.ru>
2023-04-13 14:43:11 +0300

    Проектная работа 4

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+let openButton = document.querySelector('.profile__button-edit');
+let closeButton = document.querySelector('.popup__button-close')
+
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let inputName = document.querySelector('.popup__name-input')
+let inputDescription = document.querySelector('.popup__description-input');
+
+
+function openPopup() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+  popup.classList.add('popup_opened');
+}
+openButton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popup.classList.remove('popup_opened')
+}
+
+closeButton.addEventListener('click', closePopup);
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
