289c7d12b6707b2f82b72f9249d535e2f7145b6f
Alexey Kuznetsov <i@akzntsv.ru>
2023-08-21 17:05:48 +0300

    ПР8: Правка имен инпутов

--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -6,13 +6,13 @@ export default class UserInfo {
 
   getUserInfo() {
     return {
-      name: this._profileName.textContent,
+      username: this._profileName.textContent,
       description: this._profileDescription.textContent
     }
   }
 
   setUserInfo(userData) {
-    this._profileName.textContent = userData.name;
+    this._profileName.textContent = userData.username;
     this._profileDescription.textContent = userData.description;
   }
 }
