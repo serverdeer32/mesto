8e450006bd6e6da417c363942ed6f26faf622979
Alexey Kuznetsov <i@akzntsv.ru>
2023-08-21 17:01:45 +0300

    ПР8: правка имён инпутов

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
-import './pages/index.css';
+import '../pages/index.css';
 
-import { Card } from "./scripts/components/Card.js"
+import { Card } from "./components/Card.js"
 import { formValidatorConfig,
   initialCards,
   editButton,
@@ -13,12 +13,12 @@ import { formValidatorConfig,
   popupProfile,
   formCard,
   galleryList
-} from "./scripts/utils/constants.js";
-import FormValidator from "./scripts/components/FormValidator.js";
-import Section from './scripts/components/Section.js';
-import PopupWithImage from "./scripts/components/PopupWithImage.js";
-import UserInfo from "./scripts/components/UserInfo.js";
-import PopupWithForm from "./scripts/components/PopupWithForm.js";
+} from "./utils/constants.js";
+import FormValidator from "./components/FormValidator.js";
+import Section from './components/Section.js';
+import PopupWithImage from "./components/PopupWithImage.js";
+import UserInfo from "./components/UserInfo.js";
+import PopupWithForm from "./components/PopupWithForm.js";
 
 const formEditProfile = new FormValidator(formValidatorConfig, popupProfile);
 formEditProfile.enableValidation();
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: { main: './src/scripts/index.js' },
     output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
